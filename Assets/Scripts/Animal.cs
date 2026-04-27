4ded0fdd83df966b6e3c7561bd4efba492822312
AdrienS <ascenium@gmail.com>
2026-04-27 18:54:52 +0900

    added needed polymorphism and such, the game itself needs alot of polish to be acceptable

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -5,6 +5,21 @@ public class Animal : MonoBehaviour
     
     private Rigidbody rigidBody;
 
+    private float m_JumpForce;
+
+    //ENCAPSULATE
+    public float JumpForce
+    { get   {return m_JumpForce;} 
+        set {
+            if(value >0)
+            {
+                m_JumpForce = value;        
+            }
+            else
+                Debug.Log("Cannot set negative or null jump force value!");
+        } 
+    }
+
     private bool isOnGround = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,17 +34,19 @@ public class Animal : MonoBehaviour
         Move();
     }
 
+    //ABSTRACTION
     private void Move()
     {
        transform.Rotate(Vector3.up, Input.GetAxis("Horizontal") * 40.0f * Time.deltaTime);
        rigidBody.linearVelocity = transform.forward * Input.GetAxis("Vertical") * 5.0f ;  
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
-            Jump(300.0f);
+            Jump(m_JumpForce);
         }
     }
 
-    private void Jump(float jumpForce)
+    //ABSTRACTION + Polymorphism
+    protected virtual void Jump(float jumpForce)
     {
         rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         isOnGround = false;
--- Assets/Scripts/Fox.cs
+++ Assets/Scripts/Fox.cs
@@ -3,5 +3,9 @@ using UnityEngine;
 //INHERITANCE
 public class Fox : Animal
 {
-    
+    //POLYMORPHISM
+    protected override void Jump(float jumpForce)
+    {
+        base.Jump(jumpForce + 40f);
+    }
 }
